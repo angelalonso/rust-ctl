3830de449176d1691270ed4b0fa256daf01ef413
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-06-21 09:34:26 +0200

    loading YAML works

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust CTL
+
+A flexible api server that defines object structure from a yaml file, and maps it to routes and a MySQL DB.
+
+
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate serde_yaml;
+
+#[macro_use]
+extern crate serde_derive;
+
+use std::fs::File;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Attribute {
+    title: String,
+    #[serde(skip_serializing_if="Option::is_none")]
+    mysql_scheme: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    command: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Table {
+    object_type: String,
+    attributes: Vec<Attribute>,
+    //attributes: Attribute,
+}
+
+fn main() {
+    let f = File::open("objects.yaml").unwrap();
+    let person: Table = serde_yaml::from_reader(f).unwrap();
+    println!("{:?}", person)
+}
